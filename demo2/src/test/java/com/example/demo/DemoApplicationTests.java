722fbade8ffa7bdc99776a0ef82cbcdf09297b63
chmzh <28458942@qq.com>
2018-02-26 16:31:45 +0800

    Add files via upload

--- demo2/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo2/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import com.example.demo.cache.service.RedisService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +13,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DemoApplicationTests {
 
+
+
+	private Logger logger= LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	private RedisService service;
+
 	@Test
 	public void contextLoads() {
-	}
+		User user = new User();
+		user.setId(1);
+		user.setName("name11");
+		user.setAge(19);
+		service.addUser(user);
 
+		logger.info("RedisTest执行完成，return {}",service.getStudent(user.getId()).getName());
+	}
 }
