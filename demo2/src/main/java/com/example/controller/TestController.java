6be6e1667638222cc9e1613442b5613cab2fcf6d
chmzh <28458942@qq.com>
2018-02-26 15:32:40 +0800

    Add files via upload

--- demo2/src/main/java/com/example/controller/TestController.java
+++ demo2/src/main/java/com/example/controller/TestController.java
@@ -1,8 +1,11 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @auth chenmingzhou
@@ -13,5 +16,18 @@ public class TestController {
     @PostConstruct
     public void init(){
         System.out.println("abc");
+    }
+
+    @RequestMapping(value="/getSessionId")
+    @ResponseBody
+    public String getSessionId(HttpServletRequest request){
+
+        Object o = request.getSession().getAttribute("springboot");
+        if(o == null){
+            o = "spring boot 由端口"+request.getLocalPort()+"生成";
+            request.getSession().setAttribute("springboot", o);
+        }
+
+        return "端口=" + request.getLocalPort() +  " sessionId=" + request.getSession().getId() +"<br/>"+o;
     }
 }
